1fc7179e6f573641d0d9b17eeab0015ad1e1fade
Arijeet005 <arijeetpanchotia09@gmail.com>
2025-10-02 18:11:55 +0530

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
-    base: process.env.VITE_BASE_PATH || "ATC_ai" ,
+    base: "/",
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
